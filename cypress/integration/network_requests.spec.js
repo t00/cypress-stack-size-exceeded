d5dfc7792a8495280feeefe475b10f49af3c4235
Michal Turecki <turecki@gmail.com>
2021-03-24 20:24:57 +0000

    Added cy.wrap() to response.body

--- cypress/integration/network_requests.spec.js
+++ cypress/integration/network_requests.spec.js
@@ -4,7 +4,7 @@ context('Network Requests', () => {
   it('cy.request() - make an XHR request', () => {
     // https://on.cypress.io/request
     cy.request('http://localhost:8080').then(r => {
-      r.body.should('contain', 'Hello');
+      cy.wrap(r.body).should('contain', 'Hello');
     })
   })
 })
